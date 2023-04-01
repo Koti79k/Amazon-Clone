605161a1d41eb1fbe9475feb2444150c52cd9fbf
Koti79k <Koteswararao791212@gmail.com>
2023-04-01 22:16:06 +0530

    navbar section completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import MainPage from './components/Homepage/main_page';
+import NavBar from './components/Navbar/navbar';
+
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
+      <MainPage/>
     </div>
   );
 }
