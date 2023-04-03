f9ead7d5e41e0e839ac06f6e3f09144709ba3b13
Koti79k <Koteswararao791212@gmail.com>
2023-04-03 20:48:19 +0530

    React Router added (Based on URL we can acess different pages)

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import NavBar from './components/Navbar/navbar';
 import MainPage from './components/Homepage/MainPage';
 import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 import Checkout from './components/Checkout/Checkout';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"; //v5.reactrouter.com/web/guides/quick-start
 
 
-function App() {
+
+function App() { 
   return (
-    <div className="App">
-      <NavBar />
-      {/* <MainPage /> */}
-      {/* <DisplayContent/> */}
-      {/* <PlaceOrder/> */}
-      <Checkout/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route path="/display">{<DisplayContent />}</Route>
+          <Route path="/order">{<PlaceOrder />}</Route>
+          <Route path="/checkout">{<Checkout />}</Route>
+          <Route path="">{<MainPage />}</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
