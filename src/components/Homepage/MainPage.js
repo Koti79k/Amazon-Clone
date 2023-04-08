88be61848c0317de54e718b06a3a9d19c5b614c8
Koti79k <Koteswararao791212@gmail.com>
2023-04-08 17:11:34 +0530

    home page modified

--- src/components/Homepage/MainPage.js
+++ src/components/Homepage/MainPage.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./MainPage.css";
-import AdvertisementFour from './AdvertisementFour/AdvertisementFour';
-import AdvertisementOne from './AdvertisementOne/AdvertisementOne';
+import AdvertisementFour from "./AdvertisementFour/AdvertisementFour";
+import AdvertisementOne from "./AdvertisementOne/AdvertisementOne";
 import { Link } from "react-router-dom";
 
 class MainPage extends Component {
@@ -14,14 +14,6 @@ class MainPage extends Component {
   componentDidMount() {
     let arr = [
       {
-        type: "one",
-        header: "Up to 40% off | Home & Kitchen",
-        definition: [
-          "https://ik.imagekit.io/amazon1234/box1_3_RvMp5Dkcd.jpg?updatedAt=1627118732916",
-          "https://ik.imagekit.io/amazon1234/box1_3_RvMp5Dkcd.jpg?updatedAt=1627118732916",
-        ],
-      },
-      {
         type: "four",
         header: "Shop by Category",
         definition: [
@@ -32,53 +24,67 @@ class MainPage extends Component {
         ],
       },
       {
-        type: "one",
-        header: "Up to 70% off | Electronics clearance store",
+        type: "four",
+        header: "Up to 80% off | Electronics Gadgets",
         definition: [
-          "https://ik.imagekit.io/amazon1234/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204__nBkAcb2wW.jpg?updatedAt=1627731880089",
-        ],
-      },
-      {
-        type: "one",
-        header: "Up to 70% off | Electronics clearance store",
-        definition: [
-          "https://ik.imagekit.io/amazon1234/box2_4_b005M-CUn.jpg?updatedAt=1627118750081",
-        ],
-      },
-      {
-        type: "one",
-        header: "Up to 70% off | Electronics clearance store",
-        definition: [
-          "https://ik.imagekit.io/amazon1234/box4_1_heFiuUTQ5.jpg?updatedAt=1627118751289",
+          "https://ik.imagekit.io/a7tcqauqn/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680099277169",
+          "https://ik.imagekit.io/a7tcqauqn/amazon-image/mobiles/71ZOtNdaZCL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680099277330",
+          "https://ik.imagekit.io/a7tcqauqn/amazon-image/mobiles/71ZOtNdaZCL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680099277330",
+          "https://ik.imagekit.io/a7tcqauqn/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680099277169",
         ],
       },
       {
         type: "four",
-        header: "Up to 30% off on Cooking essentials | Amazon Fresh",
+        header: "Up to 70% off | Electronics clearance store",
         definition: [
-          "https://ik.imagekit.io/amazon1234/box4_2_j7cfsH163.jpg?updatedAt=1627118749199",
-          "https://ik.imagekit.io/amazon1234/tab9_VVlk1J1_x.jpg?updatedAt=1627118745782",
-          "https://ik.imagekit.io/amazon1234/box2_1_Hdj80YRHw.jpg?updatedAt=1627118747528",
-          "https://ik.imagekit.io/amazon1234/box2_3_1Hme7yOEM.jpg?updatedAt=1627118742182",
+          "https://ik.imagekit.io/a7tcqauqn/amazon-image/mobiles/71LRBr1aLNS._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680099273153",
+          "https://ik.imagekit.io/a7tcqauqn/amazon-image/mobiles/61PDbUd1VaL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680099272518",
+          "https://ik.imagekit.io/a7tcqauqn/amazon-image/mobiles/61PDbUd1VaL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680099272518",
+          "https://ik.imagekit.io/a7tcqauqn/amazon-image/mobiles/71LRBr1aLNS._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680099273153",
         ],
       },
-      {
-        type: "four",
-        header: "Inspired by your Wish List",
-        definition: [
-          "https://ik.imagekit.io/amazon1234/tab8_oJnWNwvIX.jpg?updatedAt=1627118746586",
-          "https://ik.imagekit.io/amazon1234/box2_3_1Hme7yOEM.jpg?updatedAt=1627118742182",
-          "https://ik.imagekit.io/amazon1234/box1_4_yVnP7knQNO.jpg?updatedAt=1627118740078",
-          "https://ik.imagekit.io/amazon1234/tab10_dcLpRR5LD.jpg?updatedAt=1627118739258",
-        ],
-      },
-      {
-        type: "one",
-        header: "Festive latest launches",
-        definition: [
-          "https://ik.imagekit.io/amazon1234/box1_3_RvMp5Dkcd.jpg?updatedAt=1627118732916",
-        ],
-      },
+
+      // {
+      //   type: "one",
+      //   header: "Up to 70% off | Electronics clearance store",
+      //   definition: [
+      //     "https://ik.imagekit.io/amazon1234/box2_4_b005M-CUn.jpg?updatedAt=1627118750081",
+      //   ],
+      // },
+      // {
+      //   type: "one",
+      //   header: "Up to 70% off | Electronics clearance store",
+      //   definition: [
+      //     "https://ik.imagekit.io/amazon1234/box4_1_heFiuUTQ5.jpg?updatedAt=1627118751289",
+      //   ],
+      // },
+      // {
+      //   type: "four",
+      //   header: "Up to 30% off on Cooking essentials | Amazon Fresh",
+      //   definition: [
+      //     "https://ik.imagekit.io/amazon1234/box4_2_j7cfsH163.jpg?updatedAt=1627118749199",
+      //     "https://ik.imagekit.io/amazon1234/tab9_VVlk1J1_x.jpg?updatedAt=1627118745782",
+      //     "https://ik.imagekit.io/amazon1234/box2_1_Hdj80YRHw.jpg?updatedAt=1627118747528",
+      //     "https://ik.imagekit.io/amazon1234/box2_3_1Hme7yOEM.jpg?updatedAt=1627118742182",
+      //   ],
+      // },
+      // {
+      //   type: "four",
+      //   header: "Inspired by your Wish List",
+      //   definition: [
+      //     "https://ik.imagekit.io/amazon1234/tab8_oJnWNwvIX.jpg?updatedAt=1627118746586",
+      //     "https://ik.imagekit.io/amazon1234/box2_3_1Hme7yOEM.jpg?updatedAt=1627118742182",
+      //     "https://ik.imagekit.io/amazon1234/box1_4_yVnP7knQNO.jpg?updatedAt=1627118740078",
+      //     "https://ik.imagekit.io/amazon1234/tab10_dcLpRR5LD.jpg?updatedAt=1627118739258",
+      //   ],
+      // },
+      // {
+      //   type: "one",
+      //   header: "Festive latest launches",
+      //   definition: [
+      //     "https://ik.imagekit.io/amazon1234/box1_3_RvMp5Dkcd.jpg?updatedAt=1627118732916",
+      //   ],
+      // },
     ];
     this.setState({ list: arr });
   }
